45252e3b969e38227083316a7c570e59cf765fe8
nandinicloud <163637749+nandinicloud@users.noreply.github.com>
2024-07-11 23:22:53 +0530

    UPDATE

---
+++ dday-3/index.js
@@ -0,0 +1,51 @@
+// var age = 20; // 17 // 20
+
+// if (age > 20) {
+//   console.log("You are not eligible for Driving license.");
+// } else if (age == 20) {
+//   console.log("You are eligible for Learning license.");
+// } else {
+//   console.log("You are eligible for Driving license.");
+// }var age = 20; // 17 // 20
+
+
+
+// true,false,if else 
+
+
+// var age = 17;
+// console.log(age > 18);
+
+// if (age > 18) {
+//   console.log("Your age is valid for Driving License.");
+// } else if (age == 18) {
+//   console.log("you are allowed for driving license.");
+// } else if (age == 17)
+//  {
+//   console.log("Your age is 17.");
+// } else {
+//   console.log("Your age is Not valid for Driving License.");
+// }
+
+
+// SWITCH CASE
+
+
+var userSelectedFruit = "fruits";
+
+switch (userSelectedFruit) {
+  case "Apple":
+    console.log(" Apple");
+    break;
+  case "Grape":
+    console.log("Grape");
+    break;
+  case "Watermelon":
+    console.log("Watermelon");
+    break;
+  case "Banana":
+    console.log(" Banana");
+    break;
+  default:
+    console.log(" fruit is available");
+}
